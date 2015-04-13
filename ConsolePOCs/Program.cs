104d6b844ab13d1400e1841e207d03011a915010
Brian Preston <brian8181@gmail.com>
2015-04-13 14:54:04 +0000

    modified   CertSigningTest deleted    CertSigningTest/bin deleted    CertSigningTest/bin/Debug deleted    CertSigningTest/bin/Debug/CertSigningTest.exe deleted    CertSigningTest/bin/Debug/CertSigningTest.exe.config deleted    CertSigningTest/bin/Debug/CertSigningTest.pdb deleted    CertSigningTest/bin/Debug/CertSigningTest.vshost.exe deleted    CertSigningTest/bin/Debug/CertSigningTest.vshost.exe.config deleted    CertSigningTest/bin/Debug/CertSigningTest.vshost.exe.manifest deleted    CertSigningTest/bin/Debug/MySql.Data.dll deleted    CertSigningTest/bin/Debug/System.Data.SQLite.dll deleted    CertSigningTest/bin/Debug/System.Data.SQLite.xml deleted    CertSigningTest/bin/Debug/Utility.dll deleted    CertSigningTest/bin/Debug/Utility.pdb deleted    CertSigningTest/obj deleted    CertSigningTest/obj/Debug deleted    CertSigningTest/obj/Debug/CertSigningTest.csproj.FileListAbsolute.txt deleted    CertSigningTest/obj/Debug/CertSigningTest.csprojResolveAssemblyReference.cache deleted    CertSigningTest/obj/Debug/CertSigningTest.exe deleted    CertSigningTest/obj/Debug/CertSigningTest.pdb deleted    CertSigningTest/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache deleted    CertSigningTest/obj/Debug/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs deleted    CertSigningTest/obj/Debug/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs deleted    CertSigningTest/obj/Debug/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs deleted    CertSigningTest/obj/Debug/TempPE modified   CertSignTest deleted    CertSignTest/bin deleted    CertSignTest/bin/Debug deleted    CertSignTest/obj deleted    CertSignTest/obj/Debug deleted    CertSignTest/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache deleted    CertSignTest/obj/Debug/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs deleted    CertSignTest/obj/Debug/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs deleted    CertSignTest/obj/Debug/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs deleted    CertSignTest/obj/Debug/TempPE added      CloudInterface modified   ConsolePOCs/ConsolePOCs.csproj modified   ConsolePOCs/Program.cs added      ConsolePOCs/RSA.cs
    git-svn-id: file:///home/brian/mig/CryptoCloud/trunk@5 209816b9-9d23-4542-9216-a3de97637ac5

--- ConsolePOCs/Program.cs
+++ ConsolePOCs/Program.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.IO;
 using Utility;
+using System.Security.Cryptography;
 
 namespace ConsolePOCs
 {
@@ -9,12 +10,79 @@ namespace ConsolePOCs
     {
         static void Main(string[] args)
         {
-            // gen keys
-            RSA.KeyPair keys = RSA.GenerateKeys();
-            // save keys
+            //// gen keys
+            //QuickRSA.KeyPair keys = QuickRSA.GenerateKeyPair();
+            //// save keys
+            //File.WriteAllBytes("c:\\tmp\\privatekey2048.xml", ASCIIEncoding.ASCII.GetBytes(keys.PrivateKey));
+            //File.WriteAllBytes("c:\\tmp\\publickey2048.xml", ASCIIEncoding.ASCII.GetBytes(keys.PublicKey));
 
-            File.WriteAllBytes("c:\\tmp\\privatekey.xml", ASCIIEncoding.ASCII.GetBytes(keys.PrivateKey));
-            File.WriteAllBytes("c:\\tmp\\publickey.xml", ASCIIEncoding.ASCII.GetBytes(keys.PublicKey));
+            QuickRSA.CreateKeyPair("c:\\tmp\\keys");
+         
+            QuickRSA rsa = new QuickRSA();
+            rsa.LoadKey(UTF32Encoding.ASCII.GetString(File.ReadAllBytes("c:\\tmp\\privatekey2048.xml")));
+
+            ////NO GO! -- byte[] data = File.ReadAllBytes("C:\\tmp\\infiles\\Desert.jpg");
+            byte[] data = ASCIIEncoding.ASCII.GetBytes("Hello World!");
+            byte[] enc_data = rsa.Encrypt(data);
+            
+            byte[] denc_data = rsa.Decrypt(enc_data);
+            string s = ASCIIEncoding.ASCII.GetString(denc_data);
+
+
+
+            byte[] signed_data = rsa.SignData(enc_data);
+            bool v = rsa.VerifyData(enc_data, signed_data);
+
+            /////////////// TEST AES_RSA_HYBRID ///////////////////
+            QuickRSA rsa1 = new QuickRSA();
+            rsa1.LoadKey(UTF32Encoding.ASCII.GetString(File.ReadAllBytes("c:\\tmp\\privatekey2048.xml")));
+            byte[] data3 = File.ReadAllBytes("C:\\tmp\\infiles\\Desert.jpg");
+            byte[] rsa_aes_data = rsa1.Encrypt_RSA_AES(data3);
+            
+
+            QuickRSA rsa2 = new QuickRSA();
+            rsa2.LoadKey(UTF32Encoding.ASCII.GetString(File.ReadAllBytes("c:\\tmp\\privatekey2048.xml")));
+            byte[] final = rsa2.Decrypt_RSA_AES(rsa_aes_data);
+            //string s2 = ASCIIEncoding.ASCII.GetString(final);
+            File.WriteAllBytes("c:\\tmp\\data66.jpg", final);
+            
+
+
+            ////////////////////////////////////////////////////////
+
+
+
+
+            //// testing
+            ////byte[] key = new byte[32];
+            //byte[] key = CryptoFunctions.GererateKey(32);
+
+            byte[] k2 = CryptoFunctions.GererateKey(32);
+            byte[] iv = CryptoFunctions.GererateKey(16);
+
+            ////test2
+            byte[] data2 = File.ReadAllBytes("C:\\tmp\\infiles\\Desert.jpg");
+            byte[] aes_enc_data = CryptoFunctions.EncryptAES(k2, data2, iv);
+            CryptoFunctions.DecryptAES(k2, aes_enc_data, iv);
+
+        }
+
+        private static byte[] GK()
+        {
+            AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+            aes.KeySize = 256;
+            aes.GenerateKey();
+            return aes.Key;
+        }
+
+        private static readonly byte[] Salt =
+                                            new byte[] { 10, 20, 30, 40, 50, 60, 70, 80 };
+
+        private static byte[] CreateKey(string password, int keyBytes = 32)
+        {
+            const int Iterations = 300;
+            var keyGenerator = new Rfc2898DeriveBytes(password, Salt, Iterations);
+            return keyGenerator.GetBytes(keyBytes);
         }
     }
 }
