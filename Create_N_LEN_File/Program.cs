d43c9889bf8c53f14119420460d854255666eabb
Brian Preston <brian8181@gmail.com>
2015-05-05 14:59:46 +0000

    trivial change
    git-svn-id: file:///home/brian/mig/CryptoCloud/trunk@44 209816b9-9d23-4542-9216-a3de97637ac5

--- Create_N_LEN_File/Program.cs
+++ Create_N_LEN_File/Program.cs
@@ -10,12 +10,11 @@ namespace Create_N_LEN_File
     {
         static void Main(string[] args)
         {
-            string s =  "ABC";
+            //string s =  "ABC";
             int max = (int)Math.Floor(Math.Pow(2, 16) / 2); 
-
+            
             StreamWriter sw = new StreamWriter("c:\\tmp\\file.txt");
     
-
             for (int i = 0; i < max; ++i) 
             {
                 int num = i % 4;
