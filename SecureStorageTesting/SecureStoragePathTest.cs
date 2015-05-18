55fbe72d14b0dd1172d467b841ddd0ad42d377a9
Brian Preston <brian8181@gmail.com>
2015-05-18 16:05:45 +0000

    modified   FileLocking_ProofOfConcept/FileLocking_ProofOfConcept.v11.suo modified   SecureStorageAll.sdf modified   SecureStorageAll.sln added      SecureStorageTesting added      SecureStorageTesting/Properties added      SecureStorageTesting/Properties/AssemblyInfo.cs added      SecureStorageTesting/SecureStoragePathTest.cs added      SecureStorageTesting/SecureStorageTesting.csproj added      SecureStorageWebService/IData.cs deleted    SecureStorageWebService/IService1.cs added      SecureStorageWebService/IStorageService.cs modified   SecureStorageWebService/SecureStorageWebService.csproj deleted    SecureStorageWebService/Service1.svc deleted    SecureStorageWebService/Service1.svc.cs added      SecureStorageWebService/StorageService.svc added      SecureStorageWebService/StorageService.svc.cs
    git-svn-id: file:///home/brian/mig/CryptoCloud/trunk@73 209816b9-9d23-4542-9216-a3de97637ac5

---
+++ SecureStorageTesting/SecureStoragePathTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SecureStorageTesting
+{
+    [TestFixture]
+    public class SecureStoragePathTest
+    {
+        [Test]
+        public void IsDirectoryTest()
+        {
+            string name = "a/";
+            bool result = SecureStorageLib.StoragePath.IsDirectory(name);
+            Assert.True(result);
+        }
+    }
+}
---
+++ SecureStorageWebService/StorageService.svc.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Utility;
+
+namespace SecureStorageWebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IStorageService
+    {
+
+        private string working_dir = "c:\\tmp\\svr\\";
+
+        #region IData Members
+
+        //public void Create(string name, byte[] data)
+        //{
+        //    File.WriteAllBytes(working_dir + name, data);
+        //}
+
+        public void CreateEmpty(string name, int len, bool random = false)
+        {
+            byte[] data = null;
+            if (random)
+            {
+                data = Utility.CryptoFunctions.GenerateRandomBytes(len);
+                File.WriteAllBytes(working_dir + name, data);
+            }
+            else
+            {
+                data = new byte[len];
+                // are they not already zero?
+                Array.Clear(data, 0, len);
+            }
+        }
+
+        public void Delete(string name)
+        {
+            File.Delete(working_dir + name);
+        }
+
+        public bool Exists(string name)
+        {
+            FileInfo fi = new FileInfo(working_dir + name);
+            return fi.Exists;
+        }
+
+        //OVERLOADING EXAMPLE
+        //[ServiceContract]
+        //public interface ICalendarService
+        //{
+        //    [OperationContract(Name = "GetScheduledEventsByDate")]
+        //    ScheduledEvent[] GetScheduledEvents(DateTime date);
+
+        //    [OperationContract(Name = "GetScheduledEventsByDateRange")]
+        //    ScheduledEvent[] GetScheduledEvents(DateTime start, DateTime end);
+        //}
+
+        /// <summary>
+        /// write date to end of file
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="data"></param>
+        public bool CreateAppend(string name, byte[] data)
+        {
+            using (FileStream fs = new FileStream(working_dir + name, FileMode.Create | FileMode.Append))
+            {
+                fs.Write(data, 0, data.Length);
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// write date to end of file
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="data"></param>
+        public bool CreateReplace(string name, byte[] data)
+        {
+            using (FileStream fs = new FileStream(working_dir + name, FileMode.Create))
+            {
+                fs.Write(data, 0, data.Length);
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// read file @ offset for len bytes
+        /// </summary>
+        /// <param name="name">file by name</param>
+        /// <param name="offset">offset to read into file</param>
+        /// <param name="len">len of data to read, 0 read all</param>
+        /// <returns>the data read</returns>
+        public byte[] Read(string name, int offset, int len)
+        {
+            if (len <= 0)
+                len = (int)GetLength(name);
+
+            using (FileStream fs = new FileStream(working_dir + name, FileMode.Open))
+            {
+                byte[] data = new byte[len];
+                fs.Seek(offset, SeekOrigin.Begin);
+                long read = fs.Read(data, 0, len);
+                if (read != len)
+                    return null;
+                return data;
+            }
+        }
+
+        public int GetCount()
+        {
+            DirectoryInfo di = new DirectoryInfo(working_dir);
+            return di.GetFileSystemInfos().Length;
+        }
+
+        public long GetLength(string name)
+        {
+            FileInfo fi = new FileInfo(working_dir + name);
+            return fi.Length;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string[] GetAllNames()
+        {
+            DirectoryInfo di = new DirectoryInfo(working_dir);
+            FileSystemInfo[] fs_infos = di.GetFileSystemInfos();
+            return GetNames(0, fs_infos.Length);
+        }
+
+        /// <summary>
+        /// return subset of filename
+        /// </summary>
+        /// <param name="idx"></param>
+        /// <param name="len"></param>
+        /// <returns></returns>
+        public string[] GetNames(int idx, int len)
+        {
+            DirectoryInfo di = new DirectoryInfo(working_dir);
+            FileSystemInfo[] fs_infos = di.GetFileSystemInfos();
+            string[] names = new string[len];
+            for (int i = idx; i < len; ++i)
+            {
+                names[i] = fs_infos[i].Name;
+            }
+
+            return names;
+        }
+
+        /// <summary>
+        /// delete all files
+        /// </summary>
+        public void DeleteAll()
+        {
+            DirectoryInfo di = new DirectoryInfo(working_dir);
+            FileSystemInfo[] fs_infos = di.GetFileSystemInfos();
+            foreach (FileSystemInfo info in fs_infos)
+            {
+                info.Delete();
+            }
+        }
+
+        /// <summary>
+        /// get sha256 of file data
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public byte[] SHA256(string name)
+        {
+            byte[] data = File.ReadAllBytes(working_dir + name);
+            return CryptoFunctions.SHA256(data);
+        }
+        #endregion
+    }
+}
