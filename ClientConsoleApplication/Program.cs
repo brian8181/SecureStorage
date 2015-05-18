5ccf6864455ddeb88d6cfa813961c2b70d79ba55
Brian Preston <brian8181@gmail.com>
2015-05-18 17:14:06 +0000

    modified   ClientConsoleApplication/Program.cs added      SecureStorageTesting/SecureStorageTesting.cs modified   SecureStorageTesting/SecureStorageTesting.csproj added      SecureStorageTesting/SecureStorageUtilityTesting.cs
    git-svn-id: file:///home/brian/mig/CryptoCloud/trunk@74 209816b9-9d23-4542-9216-a3de97637ac5

--- ClientConsoleApplication/Program.cs
+++ ClientConsoleApplication/Program.cs
@@ -10,10 +10,40 @@ namespace ClientConsoleApplication
         static void Main(string[] args)
         {
 
-            ServiceReference.IService svr = new ServiceReference.ServiceClient();
-            string s = svr.GetData(5);
-            Console.WriteLine(s);
-            Console.WriteLine(svr.Hello());
+            //10.1 * 2.099
+            float f = 00.10f;
+            byte[] buf = BitConverter.GetBytes(f);
+            int n = 101;
+            Console.WriteLine(f);
+            List<char> list = new List<char>();
+
+            foreach(byte b in buf)
+            {
+                for(int i = 0; i < 8; ++i)
+                {
+                    if ((b & (1 << i)) != 0)
+                        list.Add('1');
+                    else
+                        list.Add('0');
+                }
+            }
+
+            //for (int i = 0; i < 32; ++i)
+            //{
+            //    if (( ((int)f) & (1 << i)) != 0)
+            //        list.Add('1');
+            //    else
+            //        list.Add('0');
+            //}
+
+            list.Reverse();
+            foreach (char c in list)
+                Console.Write(c);
+            
+            //ServiceReference.IService svr = new ServiceReference.ServiceClient();
+            //string s = svr.GetData(5);
+            //Console.WriteLine(s);
+            //Console.WriteLine(svr.Hello());
             Console.Read();
         }
 
---
+++ SecureStorageTesting/SecureStorageUtilityTesting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace SecureStorageTesting
+{
+    [TestFixture]
+    public class SecureStorageUtilityTesting
+    {
+    }
+}
