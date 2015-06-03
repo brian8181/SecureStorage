b7308fc533bb1f277c7d24b8aede2f42b013b020
Brian Preston <brian8181@gmail.com>
2015-06-03 22:50:55 +0000

    change commits & todo.txt
    git-svn-id: file:///home/brian/mig/CryptoCloud/trunk@105 209816b9-9d23-4542-9216-a3de97637ac5

--- SecureStorageLib/IStorage.cs
+++ SecureStorageLib/IStorage.cs
@@ -8,13 +8,61 @@ namespace SecureStorageLib
     /// </summary>
     public interface IStorage
     {
+        /// <summary>
+        /// Create
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="data"></param>
+        /// <param name="mode"></param>
         void Create(string name, byte[] data, FileMode mode = FileMode.Create);
+
+        /// <summary>
+        /// CreateEmpty
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="len"></param>
+        /// <param name="random"></param>
         void CreateEmpty(string name, int len, bool random = false);
+
+        /// <summary>
+        /// Read
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="offset"></param>
+        /// <param name="len"></param>
+        /// <returns></returns>
         byte[] Read(string name, int offset, int len);
+
+        /// <summary>
+        /// Copy
+        /// </summary>
+        /// <param name="src_name"></param>
+        /// <param name="dst_name"></param>
         void Copy(string src_name, string dst_name);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
         void Delete(string name);
+
+        /// <summary>
+        /// GetLength
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         int GetLength(string name);
+
+        /// <summary>
+        /// Exists
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         bool Exists(string name);
+        
+        /// <summary>
+        /// DeleteAll
+        /// </summary>
         void DeleteAll();
     }
 }
