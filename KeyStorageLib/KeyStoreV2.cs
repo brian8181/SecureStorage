c3e38e04835afa52648ff3228d0306505fd3ec8a
Brian Preston <brian8181@gmail.com>
2015-09-02 16:27:30 +0000

    git-svn-id: file:///home/brian/mig/CryptoCloud/trunk@136 209816b9-9d23-4542-9216-a3de97637ac5

---
+++ KeyStorageLib/KeyStoreV2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyStorage
+{
+    public class KeyStoreV2
+    {
+        public KeyStoreV2(SecureStorageLib.ICryptography crypt)
+        {
+        }
+    }
+}
