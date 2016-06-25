28863c5392ffe8ce5e3cd31141cdc602e0f01580
Brian Preston <brian8181@gmail.com>
2016-06-25 14:37:37 +0000

    minor changes
    git-svn-id: file:///home/brian/mig/CryptoCloud/trunk@148 209816b9-9d23-4542-9216-a3de97637ac5

--- CryptographyLib/PBKDF2_SHA1.cs
+++ CryptographyLib/PBKDF2_SHA1.cs
@@ -41,9 +41,7 @@ namespace KeyStorage
             
             return full_key;
         }
-
         
-
         /// <summary>
         /// Computes the PBKDF2-SHA1 hash of a password.
         /// </summary>
