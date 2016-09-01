dd467ae1d36fb40aedae8425af99b660ffc7eb56
Brian Preston <brian8181@gmail.com>
2016-09-01 13:09:54 +0000

    add new project
    git-svn-id: file:///home/brian/mig/CryptoCloud/trunk@152 209816b9-9d23-4542-9216-a3de97637ac5

---
+++ SecureStorageSyncClient/Local.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureStorageSyncClient
+{
+    /// <summary>
+    /// represent the local file storage 
+    /// </summary>
+    public class Local
+    {
+
+    }
+}
